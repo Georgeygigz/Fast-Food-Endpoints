427e12b0c7aebf0de8e8c864326db41d9fdedb34
GeorgeyGigz <georgeymutti@gmail.com>
2018-09-27 09:43:58 +0300

    [#160812549] set up database

---
+++ app/api/v2/database.py
@@ -0,0 +1,40 @@
+import psycopg2
+import os
+from app.api.v2.dbconn import queries
+
+db_url="dbname='fastfood' user='postgres' host='localhost' port=5432 password='g@_gigz-2416'"
+
+
+def conn_db():
+	try:
+		conn=psycopg2.connect(db_url)
+	except Exception as e:
+		raise e
+	return conn
+
+def create_table():
+	conn=conn_db()
+	curr=conn.cursor()
+	try:
+		for query in queries:
+			curr.execute(query)
+		conn.commit()
+	except Exception as e:
+		print(e)
+
+
+def destory():
+	conn =conn_db()
+	curr=conn.cursor()
+	orders="DROP TABLE IF EXISTS  orders CASCADE"
+	meals="DROP TABLE IF EXISTS  meals CASCADE"
+	users="DROP TABLE IF EXISTS  users CASCADE"
+	drop_queries=[orders,meals,users]
+	try:
+		for query in drop_queries:
+			curr.execute(query)
+		conn.commit()
+	except Exception as e:
+		print(e)
+
+
---
+++ app/api/v2/dbconn.py
@@ -0,0 +1,37 @@
+import psycopg2
+import os
+
+try:
+	conn=psycopg2.connect("dbname='fastfood' user='postgres' host='localhost' port=5432 password='g@_gigz-2416'")
+	curr=conn.cursor()
+
+except Exception as e:
+	print(e)
+
+
+table_1="""CREATE TABLE IF NOT EXISTS users(
+ 	                   user_id INT ,
+ 	                   user_name VARCHAR NOT NULL,
+ 	                   email VARCHAR NOT NULL PRIMARY KEY,
+ 	                   password VARCHAR NOT NULL,
+ 	                   type VARCHAR NOT NULL);"""
+
+                    
+table_2="""CREATE TABLE IF NOT EXISTS orders(
+                       orde_id INT,
+ 	                   food_id INT PRIMARY KEY,
+ 	                   food_name VARCHAR NOT NULL,
+ 	                   description VARCHAR NOT NULL,
+ 	                   quantity INT,
+ 	                   order_date Date,
+ 	                   status VARCHAR NOT NULL);"""
+
+table_3="""CREATE TABLE IF NOT EXISTS meals(
+ 	                    meal_id INT ,
+ 	                    meal_name VARCHAR NOT NULL PRIMARY KEY,
+ 	                    price VARCHAR NOT NULL,
+ 	                    category VARCHAR UNIQUE NOT NULL,
+ 	                    description VARCHAR NOT NULL);"""
+
+queries=[table_1,table_2,table_3]
+
