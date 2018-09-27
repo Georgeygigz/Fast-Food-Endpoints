36f0588b6b22c859e76fb7cd88485c01f504c829
GeorgeyGigz <georgeymutti@gmail.com>
2018-09-27 11:27:57 +0300

    [#160812549] add model package

---
+++ app/api/v2/models.py
@@ -0,0 +1,16 @@
+class FastFood():
+    def __init__(self):
+        pass
+    
+
+    def save_users(self):
+        pass
+    
+    def save_orders(self):
+        pass
+    
+    def get_all_orders(self):
+        pass
+
+    def save_meals(self):
+        pass
