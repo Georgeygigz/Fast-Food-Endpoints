1686c6c8af12df34b3c2066ae1d5a5f009ffab4b
GeorgeyGigz <georgeymutti@gmail.com>
2018-10-02 22:53:44 +0300

    [#160838536] test for invalid url

--- app/tests/v2/test_auth_views.py
+++ app/tests/v2/test_auth_views.py
@@ -34,9 +34,49 @@ class TestApiEndpoints(unittest.TestCase):
         self.assertEqual(post_result.json,{'geoe@gmail.com': 'Aready Exist'})
         self.assertEqual(post_result.status_code,409)
     
+    '''Test test create an account'''
     def test_user_create_account(self):
         response=self.app.post('/app/v2/auth/signup',
                                 data=json.dumps(self.users),
                                 headers={'content_type':'application/json'})
         self.assertEqual(response.json,{'message':'Account created successfuly'})
         self.assertEqual(response.status_code,201)
+    
+    '''Test Invalid email'''
+    def test_invalid_email(self):
+        response=self.app.post('app/v2/auth/signup',
+                               data=json.dumps({"user_id":1,
+                                                "username":'gege',
+                                                "email":"geoegmail.com",
+                                                "password":"g#2jd#D$4Bkl",
+                                                "user_type":"user"}),
+                               headers={'content_type':'application/json'})
+        self.assertEqual(response.json,{'message': 'invalid Email'})
+        self.assertEqual(response.status_code,401)
+
+    '''Test Invalid password'''
+    def test_invalid_password(self):
+        response=self.app.post('app/v2/auth/signup',
+                               data=json.dumps({"user_id":1,
+                                                "username":'gege',
+                                                "email":"geoe@gmail.com",
+                                                "password":"g#fhb48",
+                                                "user_type":"user"}),
+                               headers={'content_type':'application/json'})
+        self.assertEqual(response.json,{'message': 'invalid password'})
+        self.assertEqual(response.status_code,401)
+    
+    def test_invalid_url(self):
+        response=self.app.post('app/v2/auth/signiup',
+                                data=json.dumps(self.users),
+                                headers={'content_type':'application/json'})
+        #self.assertEqual(response.json,{'message":"Request not found'})
+        self.assertEqual(response.status_code,404)
+    '''Test login'''
+    def test_login(self):
+        response=self.app.post('app/v2/auth/login',
+                                data=json.dumps({"email":"geoe@gmail.com","password":"g#2jd#D$4Bkl"}),
+                                headers={'content_type':'application/json'})
+        self.assertEqual(response.json,{"message":"Login succesful"})
+
+
